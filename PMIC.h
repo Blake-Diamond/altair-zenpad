79ec9b1318b567b220d9a025305eef06b03f6b5e
Blake Diamond <46721841+Blake-Diamond@users.noreply.github.com>
2019-03-16 17:58:49 -0700

    Add files via upload

---
+++ PMIC.h
@@ -0,0 +1,17 @@
+// header file for PMIC that defines constants
+
+//default register settings (decimal)
+#define REG1_value 0
+#define REG2_value 0
+#define REG3_value 136
+#define REG4_value 153
+#define REG5_value 146
+#define REG6_value 0
+#define REG7_value 0
+#define REG8_value 0
+
+//i2c settings
+#define SDA_pin 18
+#define SCL_pin 19
+#define Clock_freq 100000 //100 khz
+#define PMIC_addr 90      //7bit address of PMIC
