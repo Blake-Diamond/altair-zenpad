88d9239f505956672fae84f4b84a49d6ab4c76fc
Blake Diamond <diamond@ucsb.edu>
2019-05-19 14:03:08 -0700

    prints basic characters from app

---
+++ Winter_Demo/BluefruitConfig.h
@@ -0,0 +1,93 @@
+// COMMON SETTINGS
+// ----------------------------------------------------------------------------------------------
+// These settings are used in both SW UART, HW UART and SPI mode
+// ----------------------------------------------------------------------------------------------
+#define BUFSIZE                        128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE                   true  // If set to 'true' enables debug output
+
+#define FACTORYRESET_ENABLE         1
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+
+// SOFTWARE UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins that will be used for 'SW' serial.
+// You should use this option if you are connecting the UART Friend to an UNO
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SWUART_RXD_PIN       1    // Required for software serial!
+#define BLUEFRUIT_SWUART_TXD_PIN       0   // Required for software serial!
+#define BLUEFRUIT_UART_CTS_PIN         25   // Required for software serial!
+#define BLUEFRUIT_UART_RTS_PIN         27   // Optional, set to -1 if unused
+
+
+// HARDWARE UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the HW serial port you are using. Uncomment
+// this line if you are connecting the BLE to Leonardo/Micro or Flora
+// ----------------------------------------------------------------------------------------------
+#ifdef Serial1    // this makes it not complain on compilation if there's no Serial1
+ #define BLUEFRUIT_HWSERIAL_NAME      Serial1
+#endif
+
+
+// SHARED UART SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following sets the optional Mode pin, its recommended but not required
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_UART_MODE_PIN        26    // Set to -1 if unused
+
+
+// SHARED SPI SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins to use for HW and SW SPI communication.
+// SCK, MISO and MOSI should be connected to the HW SPI pins on the Uno when
+// using HW SPI.  This should be used with nRF51822 based Bluefruit LE modules
+// that use SPI (Bluefruit LE SPI Friend).
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SPI_CS               8
+#define BLUEFRUIT_SPI_IRQ              7
+#define BLUEFRUIT_SPI_RST              4    // Optional but recommended, set to -1 if unused
+
+// SOFTWARE SPI SETTINGS
+// ----------------------------------------------------------------------------------------------
+// The following macros declare the pins to use for SW SPI communication.
+// This should be used with nRF51822 based Bluefruit LE modules that use SPI
+// (Bluefruit LE SPI Friend).
+// ----------------------------------------------------------------------------------------------
+#define BLUEFRUIT_SPI_SCK              13
+#define BLUEFRUIT_SPI_MISO             12
+#define BLUEFRUIT_SPI_MOSI             11
+
+
+/**************************************************************************/
+/*!
+    @brief  Checks for user input (via the Serial Monitor)
+*/
+/**************************************************************************/
+bool getUserInput(char buffer[], uint8_t maxSize)
+{
+  // timeout in 100 milliseconds
+  TimeoutTimer timeout(100);
+
+  memset(buffer, 0, maxSize);
+  while( (!Serial.available()) && !timeout.expired() ) { delay(1); }
+
+  if ( timeout.expired() ) return false;
+
+  delay(2);
+  uint8_t count=0;
+  do
+  {
+    count += Serial.readBytes(buffer+count, maxSize);
+    delay(2);
+  } while( (count < maxSize) && (Serial.available()) );
+
+  return true;
+}
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
---
+++ Winter_Demo/Winter_Demo.ino
@@ -0,0 +1,182 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include <SPI.h>
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "PMIC.h"
+#include "BluefruitConfig.h"
+#include "MassagePatterns.h"
+
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
+
+ // Create the bluefruit object, either software serial...uncomment these lines
+  SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+  Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+                      
+void setup() {
+  Setup_i2c();              //i2c configuration
+  delay(1000);              //wait 1 second
+  Set_PMICReg();            //set PMIC Registers to Default Values
+
+  ReadReg(PMIC_addr,1,1);   //garbage reading 
+  Print_allReg();           //print all PMIC Registers to Serial Monitor - need to clean this up
+  
+  //Need to come up with function that checks the major errors in the PMIC using bitmasking
+
+  Setup_bluetooth();         //Configure bluetooth module
+
+  Set_pinsOutput();          //Set PWM pins as output on Teensy 3.6
+  
+  
+}
+
+
+void loop(void)
+{
+  // Check for user input
+  char inputs[BUFSIZE+1];
+
+  if ( getUserInput(inputs, BUFSIZE) )
+  {
+    // Send characters to Bluefruit
+    Serial.print("[Send] ");
+    Serial.println(inputs);
+
+    ble.print("AT+BLEUARTTX=");
+    ble.println(inputs);
+
+    // check response stastus
+    if (! ble.waitForOK() ) {
+      Serial.println(F("Failed to send?"));
+    }
+  }
+
+  // Check for incoming characters from Bluefruit
+  ble.println("AT+BLEUARTRX");
+  ble.readline();
+
+
+
+
+  //use a large switch statement here for the dynamic massage patterns
+  //maybe use 3 bytes: [Category,Motor,Speed] ie 0x01 0xA5 0xC5
+
+  //template for receiving a particular command
+  if (strcmp(ble.buffer, "OK") == 0) {
+    // no data
+    return;
+  }
+  
+
+
+  // Some data was found, its in the buffer
+  Serial.print(("[Recv] ")); //Serial.println(ble.buffer);
+  //Serial.println(ble.buffer);
+
+
+  //catches dummy command from android app
+  if(ble.buffer[0] == 0xFF){
+    for (int i = 0; i < sizeof(ble.buffer); i++) {
+      Serial.println(ble.buffer[i],HEX);
+      if (ble.buffer[i] == 0x12) {
+        break;
+      }
+
+      
+    }
+    Serial.println("FF Received!");
+    
+    Serial.println(ble.buffer[2],HEX);
+  }
+ 
+     if (strcmp(ble.buffer, "START") == 0) {
+      Serial.println("Start Recived");
+//      Drive_motors();
+//      startFlag = 1;
+      analogWrite(R6, 190);
+      delay(1000);
+
+     }
+      
+
+      if (strcmp(ble.buffer, "Stop") == 0) {
+      Serial.println("Stop Recived");
+      analogWrite(R6, 0);
+  }
+
+  
+  ble.waitForOK();
+}
+
+
+
+
+/* 
+ *  @brief   sets up bluetooth communication and configuration to serial monitor
+ *  @param   none
+ *  @return  none
+*/
+void Setup_bluetooth(void)
+{
+
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+  
+//  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Command Mode Example"));
+  Serial.println(F("---------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("******************************"));
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+    Serial.println(F("******************************"));
+  }
+}
